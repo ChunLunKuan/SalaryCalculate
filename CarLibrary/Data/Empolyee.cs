9cf86348cde22ce1e353b171f5d2d13146b623bd
ChunLunKuan <key571@msn.com>
2015-12-01 00:01:53 +0800

    薪資計算機
    底薪+獎金=總薪資

---
+++ CarLibrary/Data/Empolyee.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarLibrary.Data
+{
+    public class Empolyee
+    {
+        private int BaseSalary;
+        private int Benefit;
+        private int Salary;
+
+        public int SetBaseSalary
+        {
+            get { return BaseSalary; }
+            set
+            {
+                if (value <= 22000)
+                    BaseSalary = 22000;
+                else if (value >= 33000)
+                    BaseSalary = 30000;
+                else
+                    BaseSalary = value;
+            }
+        }
+
+        public int SetBenefit
+        {
+            get { return Benefit; }
+            set
+            {
+                if (value <= 1000)
+                    Benefit = 1000;
+                else if (value >= 10000)
+                    Benefit = 10000;
+                else
+                    Benefit = value;
+            }
+        }
+
+        public int SetSalary
+        {
+            get { return Salary = BaseSalary + Benefit; }
+            set { }
+        }
+    }
+}
---
+++ Example03/Program.cs
@@ -0,0 +1,24 @@
+﻿using CarLibrary.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example03
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Empolyee empolyee = new Empolyee();
+            Console.WriteLine("請輸入底薪");
+            int baseSalary = int.Parse(Console.ReadLine());
+            empolyee.SetBaseSalary = baseSalary;
+            Console.WriteLine("請輸入獎金");
+            int benefit = int.Parse(Console.ReadLine());
+            empolyee.SetBenefit = benefit;
+            Console.WriteLine(empolyee.SetSalary);
+        }
+    }
+}
